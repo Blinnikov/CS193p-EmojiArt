0a024b436b0c491d268d5437bab5c11bdf2b88c6
Igor Blinnikov <igor.blinnikov@gmail.com>
2022-01-12 11:53:43 +0300

    feat(emojiart): init

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Igor Blinnikov on 12.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
