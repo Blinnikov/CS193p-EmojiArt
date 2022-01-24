68e81c31bf6debfaf9604c33cf42bdb514bb38af
Igor Blinnikov <igor.blinnikov@gmail.com>
2022-01-24 15:40:41 +0300

    feat(emojiart): lecture 12 - palette editor completed

--- EmojiArt/PaletteEditor.swift
+++ EmojiArt/PaletteEditor.swift
@@ -12,15 +12,59 @@ struct PaletteEditor: View {
   
   var body: some View {
     Form {
-      TextField("Name", text: $palette.name)
+      nameSection
+      addEmojiSection
+      removeEmojiSection
     }
     .frame(minWidth: 300, minHeight: 350)
   }
+  
+  var nameSection: some View {
+    Section(header: Text("Name")) {
+      TextField("Name", text: $palette.name)
+    }
+  }
+  
+  @State private var emojisToAdd = ""
+  
+  var addEmojiSection: some View {
+    Section(header: Text("Add Emojis")) {
+      TextField("", text: $emojisToAdd)
+        .onChange(of: emojisToAdd) { emojis in
+          addEmojis(emojis)
+        }
+    }
+  }
+  
+  func addEmojis(_ emojis: String) {
+    withAnimation {
+      palette.emojis = (emojis + palette.emojis)
+        .filter { $0.isEmoji }
+        .removingDuplicateCharacters
+    }
+  }
+  
+  var removeEmojiSection: some View {
+    Section(header: Text("Remove Emoji")) {
+      let emojis = palette.emojis.removingDuplicateCharacters.map { String($0) }
+      LazyVGrid(columns: [GridItem(.adaptive(minimum: 40))]) {
+        ForEach(emojis, id: \.self) { emoji in
+          Text(emoji)
+            .onTapGesture {
+              withAnimation {
+                palette.emojis.removeAll(where: { String($0) == emoji })
+              }
+            }
+        }
+      }
+      .font(.system(size: 40))
+    }
+  }
 }
 
-//struct PaletteEditor_Previews: PreviewProvider {
-//  static var previews: some View {
-//    PaletteEditor()
-//      .previewLayout(.fixed(width: 300, height: 350))
-//  }
-//}
+struct PaletteEditor_Previews: PreviewProvider {
+  static var previews: some View {
+    PaletteEditor(palette: .constant(PaletteStore(named: "Preview").palette(at: 4)))
+      .previewLayout(.fixed(width: 300, height: 350))
+  }
+}
